5dcc9fb99338a7bb9824260187e55bbd224a3f5c
12116809 <lestianm@gmail.com>
2023-03-14 20:19:09 -0300

    feat: implementa janela

---
+++ src/jframe/FramePanel.java
@@ -0,0 +1,19 @@
+package jframe;
+
+import javax.swing.*;
+
+public class FramePanel {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame("Meu primeiro frame em Java");
+        JButton botao = new JButton("Botão do Swing");
+        JLabel texto = new JLabel("Número de clicks: 0");
+
+        JPanel painel = new JPanel();
+        painel.add(botao);
+        painel.add(texto);
+        janela.getContentPane().add(painel);
+        janela.setSize(300,200);
+        janela.show();
+
+    }
+}
---
+++ src/valoresInteiros/SaidaValoresInteiros.java
@@ -0,0 +1,15 @@
+package valoresInteiros;
+
+import javax.swing.*;
+
+public class SaidaValoresInteiros {
+    public static void main(String[]args) throws NumberFormatException{
+
+        int valorA = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor A: "));
+        int valorB = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor B: "));
+
+        int resultado = valorA * valorB;
+
+        JOptionPane.showMessageDialog(null,"Resultado final: "+ resultado, "Resultado ",JOptionPane.WARNING_MESSAGE);
+    }
+}
